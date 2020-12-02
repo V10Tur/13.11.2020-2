978d834c5ede720f16a5420768c1f6782ae9c36f
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:54:44 +0300

    Create 13.11.2020-2.java

---
+++ 13.11.2020-2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class dfg {
+    package study;
+
+import java.util.Scanner; // импорт класса Scanner
+
+    public class Program {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+        }
+    }
+
+}
